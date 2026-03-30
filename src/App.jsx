8ad4adf1f3aa8cdcf6e636e0b469568c58011a1c
Oluwatoye <ebijhimi@gmail.com>
2026-03-30 12:41:39 +0100

    create Hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from "./components/Navbar"
+import Hero from './components/Hero'
 
 function App() {
   
 
   return (
-  
+  <>
   <Navbar/>  
+<Hero />
 
+</>
   )
 }
 
